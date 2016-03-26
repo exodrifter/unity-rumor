d68b49085e5ff2f51447c0ddf01fd2e11a70b50a
Ava Pek <exodrifter@gmail.com>
2016-03-26 17:11:43 -0500

    Add Rumor and Yield tests

---
+++ Editor/Tests/RumorTest.cs
@@ -0,0 +1,97 @@
+﻿using Exodrifter.Rumor.Nodes;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace Exodrifter.Rumor.Test
+{
+	/// <summary>
+	/// Makes sure that Rumor objects operate as expected.
+	/// </summary>
+	public class RumorTest
+	{
+		/// <summary>
+		/// Makes sure empty Rumor objects operate without exceptions.
+		/// </summary>
+		[Test]
+		public void EmptyRumor()
+		{
+			var rumor = new Engine.Rumor(new List<Node>());
+
+			var iter = rumor.Run();
+			iter.MoveNext();
+
+			Assert.True(rumor.Started);
+			Assert.True(rumor.Finished);
+
+			Assert.DoesNotThrow(() => rumor.Update(0));
+			Assert.DoesNotThrow(rumor.Advance);
+		}
+
+		/// <summary>
+		/// Makes sure a simple Rumor object can operate without exceptions.
+		/// </summary>
+		[Test]
+		public void SimpleRumor()
+		{
+			var rumor = new Engine.Rumor(new List<Node>() {
+				new Dialog("a"),
+				new Dialog("b"),
+			});
+
+			var iter = rumor.Run();
+
+			Assert.False(rumor.Started);
+			Assert.False(rumor.Finished);
+
+			iter.MoveNext();
+
+			Assert.True(rumor.Started);
+			Assert.False(rumor.Finished);
+
+			Assert.DoesNotThrow(() => rumor.Update(0));
+			Assert.DoesNotThrow(rumor.Advance);
+			iter.MoveNext();
+
+			Assert.True(rumor.Started);
+			Assert.False(rumor.Finished);
+
+			Assert.DoesNotThrow(() => rumor.Update(0));
+			Assert.DoesNotThrow(rumor.Advance);
+			iter.MoveNext();
+
+			Assert.True(rumor.Started);
+			Assert.True(rumor.Finished);
+		}
+
+		/// <summary>
+		/// Makes sure Rumor objects do not throw exceptions when events are
+		/// passed to it while it is not running
+		/// </summary>
+		[Test]
+		public void RumorEventWithoutStart()
+		{
+			var rumor = new Engine.Rumor(new List<Node>());
+
+			Assert.DoesNotThrow(rumor.Advance);
+			Assert.DoesNotThrow(() => rumor.Update(0));
+		}
+
+		/// <summary>
+		/// Makes sure Rumor objects throw an exception when run is invoked
+		/// before the previous run has completed.
+		/// </summary>
+		[Test]
+		public void RumorDisallowMultipleRun()
+		{
+			var rumor = new Engine.Rumor(new List<Node>() {
+				new Dialog("a")
+			});
+
+			rumor.Run().MoveNext();
+			Assert.Throws<InvalidOperationException>(
+				() => rumor.Run().MoveNext()
+			);
+		}
+	}
+}
---
+++ Editor/Tests/YieldTest.cs
@@ -0,0 +1,52 @@
+﻿using Exodrifter.Rumor.Engine;
+using NUnit.Framework;
+
+namespace Exodrifter.Rumor.Test
+{
+	/// <summary>
+	/// Makes sure that yield objects operate as expected.
+	/// </summary>
+	public class YieldTest
+	{
+		/// <summary>
+		/// Makes sure yield for seconds operates as expected.
+		/// </summary>
+		[Test]
+		public void YieldSeconds()
+		{
+			var yield = new ForSeconds(1);
+			Assert.False(yield.Finished);
+
+			yield.OnAdvance();
+			Assert.False(yield.Finished);
+
+			yield.OnUpdate(0);
+			Assert.False(yield.Finished);
+
+			yield.OnUpdate(1);
+			Assert.True(yield.Finished);
+
+			yield.OnUpdate(1);
+			Assert.True(yield.Finished);
+		}
+
+		/// <summary>
+		/// Makes sure yield for advance operates as expected.
+		/// </summary>
+		[Test]
+		public void YieldAdvance()
+		{
+			var yield = new ForAdvance();
+			Assert.False(yield.Finished);
+
+			yield.OnUpdate(0);
+			Assert.False(yield.Finished);
+
+			yield.OnAdvance();
+			Assert.True(yield.Finished);
+
+			yield.OnAdvance();
+			Assert.True(yield.Finished);
+		}
+	}
+}
