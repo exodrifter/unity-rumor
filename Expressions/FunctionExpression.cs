9764f568b65289a544b595c4309b8cc3854ec070
Ava Pek <exodrifter@gmail.com>
2016-09-19 04:55:03 -0500

    Initial FunctionExpression

---
+++ Expressions/FunctionExpression.cs
@@ -0,0 +1,98 @@
+﻿using Exodrifter.Rumor.Engine;
+using Exodrifter.Rumor.Util;
+using System;
+using System.Runtime.Serialization;
+using System.Collections.Generic;
+
+namespace Exodrifter.Rumor.Expressions
+{
+	/// <summary>
+	/// Represents an expression that is a call to a function.
+	/// </summary>
+	[Serializable]
+	public class FunctionExpression : Expression, ISerializable
+	{
+		/// <summary>
+		/// The name of the function to call.
+		/// </summary>
+		public string Name { get { return name; } }
+		private readonly string name;
+
+		private readonly List<Expression> @params;
+
+		public FunctionExpression(string name)
+		{
+			this.name = name;
+			this.@params = new List<Expression>();
+		}
+
+		public FunctionExpression(string name, IEnumerable<Expression> @params)
+		{
+			this.name = name;
+			this.@params = new List<Expression>(@params);
+		}
+
+		public override Value Evaluate(Scope scope)
+		{
+			// TODO: Call binding and return values properly
+			var result = Engine.Rumor.CallBinding(name);
+			return null;
+		}
+
+		public override string ToString()
+		{
+			return name;
+		}
+
+		#region Equality
+
+		public override bool Equals(object obj)
+		{
+			var other = obj as FunctionExpression;
+			return Equals(other);
+		}
+
+		public bool Equals(FunctionExpression other)
+		{
+			if (other == null) {
+				return false;
+			}
+			if (@params.Count != other.@params.Count) {
+				return false;
+			}
+
+			for (int i = 0; i < @params.Count; ++i) {
+				if (@params[i] != other.@params[i]) {
+					return false;
+				}
+			}
+
+			return other.name == name;
+		}
+
+		public override int GetHashCode()
+		{
+			return name.GetHashCode();
+		}
+
+		#endregion
+
+		#region Serialization
+
+		public FunctionExpression
+			(SerializationInfo info, StreamingContext context)
+		{
+			name = info.GetValue<string>("name");
+			@params = info.GetValue<List<Expression>>("params");
+		}
+
+		void ISerializable.GetObjectData
+			(SerializationInfo info, StreamingContext context)
+		{
+			info.AddValue<string>("name", name);
+			info.AddValue<List<Expression>>("params", @params);
+		}
+
+		#endregion
+	}
+}
