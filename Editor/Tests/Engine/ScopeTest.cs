fd61aaf05a0523cd5290166416c56f825053fdfe
Ava Pek <exodrifter@gmail.com>
2018-01-04 14:37:39 -0600

    Scope unit tests

---
+++ Editor/Tests/Engine/ScopeTest.cs
@@ -0,0 +1,132 @@
+﻿#if UNITY_EDITOR
+
+using Exodrifter.Rumor.Engine;
+using NUnit.Framework;
+
+namespace Exodrifter.Rumor.Test.Engine
+{
+	/// <summary>
+	/// Ensure Scope objects operate as expected.
+	/// </summary>
+	public class ScopeTest
+	{
+		#region Events
+
+		[Test]
+		public void OnClearTest()
+		{
+			var success = false;
+			var scope = new Scope();
+			scope.OnSet += (name, value) => Assert.Fail();
+			scope.OnClear += () =>
+			{
+				success = true;
+			};
+
+			scope.ClearVars();
+			Assert.IsTrue(success);
+		}
+
+		[Test]
+		public void OnSetTest()
+		{
+			var scope = new Scope();
+			scope.OnClear += () => Assert.Fail();
+			scope.OnSet += (name, value) =>
+			{
+				Assert.AreEqual("foo", name);
+				Assert.IsTrue(value.IsBool());
+				Assert.IsTrue(value.AsBool());
+			};
+
+			scope.SetVar("foo", true);
+		}
+
+		#endregion
+
+		#region Setters
+
+		[Test]
+		public void SetBoolTest()
+		{
+			var scope = new Scope();
+
+			scope.SetVar("foo", true);
+			Assert.IsTrue(scope.GetVar("foo").IsBool());
+			Assert.IsTrue(scope.GetVar("foo").AsBool());
+
+			scope.SetVar("foo", false);
+			Assert.IsTrue(scope.GetVar("foo").IsBool());
+			Assert.IsFalse(scope.GetVar("foo").AsBool());
+		}
+
+		[Test]
+		public void SetIntTest()
+		{
+			var scope = new Scope();
+
+			scope.SetVar("foo", 1);
+			Assert.IsTrue(scope.GetVar("foo").IsInt());
+			Assert.AreEqual(1, scope.GetVar("foo").AsInt());
+
+			scope.SetVar("foo", -100);
+			Assert.IsTrue(scope.GetVar("foo").IsInt());
+			Assert.AreEqual(-100, scope.GetVar("foo").AsInt());
+		}
+
+		[Test]
+		public void SetFloatTest()
+		{
+			var scope = new Scope();
+
+			scope.SetVar("foo", 1.0f);
+			Assert.IsTrue(scope.GetVar("foo").IsFloat());
+			Assert.AreEqual(1.0f, scope.GetVar("foo").AsFloat());
+
+			scope.SetVar("foo", -100.0f);
+			Assert.IsTrue(scope.GetVar("foo").IsFloat());
+			Assert.AreEqual(-100.0f, scope.GetVar("foo").AsFloat());
+		}
+
+		[Test]
+		public void SetStringTest()
+		{
+			var scope = new Scope();
+
+			scope.SetVar("foo", "bar");
+			Assert.IsTrue(scope.GetVar("foo").IsString());
+			Assert.AreEqual("bar", scope.GetVar("foo").AsString());
+		}
+
+		[Test]
+		public void SetObjectTest()
+		{
+			var scope = new Scope();
+
+			var obj = new object();
+			scope.SetVar("foo", obj);
+			Assert.IsTrue(scope.GetVar("foo").IsObject());
+			Assert.AreEqual(obj, scope.GetVar("foo").AsObject());
+
+			scope.SetVar("foo", (object)true);
+			Assert.IsTrue(scope.GetVar("foo").IsBool());
+			Assert.IsTrue(scope.GetVar("foo").AsBool());
+
+			scope.SetVar("foo", (object)1);
+			Assert.IsTrue(scope.GetVar("foo").IsInt());
+			Assert.AreEqual(1, scope.GetVar("foo").AsInt());
+
+			scope.SetVar("foo", (object)1.0f);
+			Assert.IsTrue(scope.GetVar("foo").IsFloat());
+			Assert.AreEqual(1.0f, scope.GetVar("foo").AsFloat());
+
+			scope.SetVar("foo", (object)"bar");
+			Assert.IsTrue(scope.GetVar("foo").IsString());
+			Assert.AreEqual("bar", scope.GetVar("foo").AsString());
+		}
+
+		#endregion
+	}
+}
+
+#endif
--- Engine/Scope.cs
+++ Engine/Scope.cs
@@ -7,39 +7,89 @@ using System.Runtime.Serialization;
 namespace Exodrifter.Rumor.Engine
 {
 	/// <summary>
-	/// A scope which keeps track of declared variables and functions. While
-	/// variables are serialized, bindings are NOT serialized.
+	/// Keeps track of declared variables and the default speaker.
 	/// </summary>
 	[Serializable]
 	public class Scope : ISerializable
 	{
+		#region Properties
+
 		/// <summary>
-		/// The variables in this scope.
+		/// The declared variables.
 		/// </summary>
 		private readonly Dictionary<string, Value> vars;
 
 		/// <summary>
-		/// The default narrator for nodes to use.
+		/// The default speaker.
 		/// </summary>
 		public object DefaultSpeaker { get; set; }
+
+		#endregion
+
+		#region Events
+
+		/// <summary>
+		/// An event that is called when all declared variables are removed.
+		/// </summary>
+		public event Action OnClear;
 
 		/// <summary>
 		/// An event that is called when a variable is set.
 		/// </summary>
-		public event Action OnVarSet;
+		public event Action<string, Value> OnSet;
+
+		#endregion
+
+		#region Constructor
 
 		/// <summary>
-		/// Creates a new scope.
+		/// Initializes a new instance of this class.
 		/// </summary>
-		/// <param name="parent">
-		/// The parent scope or null if there is none.
-		/// </param>
 		public Scope()
 		{
 			vars = new Dictionary<string, Value>();
 		}
 
+		#endregion
+
 		#region Variables
+
+		/// <summary>
+		/// Removes all declared variables.
+		/// </summary>
+		public void ClearVars()
+		{
+			vars.Clear();
+
+			if (OnClear != null)
+			{
+				OnClear();
+			}
+		}
+
+		/// <summary>
+		/// Returns the value of the specified variable.
+		/// </summary>
+		/// <returns>The value of the variable.</returns>
+		/// <param name="name">The name of the variable to get.</param>
+		public Value GetVar(string name)
+		{
+			if (vars.ContainsKey(name))
+			{
+				return vars[name];
+			}
+			return null;
+		}
+
+		/// <summary>
+		/// Returns true if the specified variable is declared.
+		/// </summary>
+		/// <param name="name">The name of the variable to find.</param>
+		/// <returns>True if the variable is declared.</returns>
+		public bool HasVar(string name)
+		{
+			return vars.ContainsKey(name);
+		}
 
 		/// <summary>
 		/// Sets the value of one variable to a bool.
@@ -50,8 +100,9 @@ namespace Exodrifter.Rumor.Engine
 		{
 			vars[name] = new BoolValue(@bool);
 
-			if (OnVarSet != null) {
-				OnVarSet();
+			if (OnSet != null)
+			{
+				OnSet(name, vars[name]);
 			}
 		}
 
@@ -64,8 +115,9 @@ namespace Exodrifter.Rumor.Engine
 		{
 			vars[name] = new IntValue(@int);
 
-			if (OnVarSet != null) {
-				OnVarSet();
+			if (OnSet != null)
+			{
+				OnSet(name, vars[name]);
 			}
 		}
 
@@ -78,8 +130,9 @@ namespace Exodrifter.Rumor.Engine
 		{
 			vars[name] = new FloatValue(@float);
 
-			if (OnVarSet != null) {
-				OnVarSet();
+			if (OnSet != null)
+			{
+				OnSet(name, vars[name]);
 			}
 		}
 
@@ -92,27 +145,48 @@ namespace Exodrifter.Rumor.Engine
 		{
 			vars[name] = new StringValue(@string);
 
-			if (OnVarSet != null) {
-				OnVarSet();
+			if (OnSet != null)
+			{
+				OnSet(name, vars[name]);
 			}
 		}
 
 		/// <summary>
-		/// Sets the value of one variable to another.
+		/// Sets the value of one variable.
 		/// </summary>
 		/// <param name="name">The name of the variable to set.</param>
 		/// <param name="value">The value of the variable to use.</param>
 		public void SetVar(string name, object @object)
 		{
-			vars[name] = new ObjectValue(@object);
+			if (@object is bool)
+			{
+				vars[name] = new BoolValue((bool)@object);
+			}
+			else if (@object is int)
+			{
+				vars[name] = new IntValue((int)@object);
+			}
+			else if (@object is float)
+			{
+				vars[name] = new FloatValue((float)@object);
+			}
+			else if (@object is string)
+			{
+				vars[name] = new StringValue((string)@object);
+			}
+			else
+			{
+				vars[name] = new ObjectValue(@object);
+			}
 
-			if (OnVarSet != null) {
-				OnVarSet();
+			if (OnSet != null)
+			{
+				OnSet(name, vars[name]);
 			}
 		}
 
 		/// <summary>
-		/// Sets the value of one variable to another.
+		/// Sets the value of one variable.
 		/// </summary>
 		/// <param name="name">The name of the variable to set.</param>
 		/// <param name="value">The value of the variable to use.</param>
@@ -120,41 +194,10 @@ namespace Exodrifter.Rumor.Engine
 		{
 			vars[name] = value;
 
-			if (OnVarSet != null) {
-				OnVarSet();
+			if (OnSet != null)
+			{
+				OnSet(name, vars[name]);
 			}
-		}
-
-		/// <summary>
-		/// Returns the value of the specified variable.
-		/// </summary>
-		/// <returns>The value of the variable.</returns>
-		/// <param name="name">The name of the variable to get.</param>
-		public Value GetVar(string name)
-		{
-			if (vars.ContainsKey(name)) {
-				return vars[name];
-			}
-			return null;
-		}
-
-		/// <summary>
-		/// Returns true if this scope or a parent scope has the specified
-		/// variable.
-		/// </summary>
-		/// <param name="name">The name of the variable to find.</param>
-		/// <returns>True if the variable is declared.</returns>
-		public bool HasVar(string name)
-		{
-			return vars.ContainsKey(name);
-		}
-
-		/// <summary>
-		/// Removes all of the variables from this scope.
-		/// </summary>
-		public void ClearVars()
-		{
-			vars.Clear();
 		}
 
 		#endregion
@@ -167,7 +210,8 @@ namespace Exodrifter.Rumor.Engine
 			var values = info.GetValue<List<Value>>("values");
 
 			vars = new Dictionary<string, Value>();
-			for (int i = 0; i < keys.Count; ++i) {
+			for (int i = 0; i < keys.Count; ++i)
+			{
 				vars.Add(keys[i], values[i]);
 			}
 
@@ -180,7 +224,8 @@ namespace Exodrifter.Rumor.Engine
 			var keys = new List<string>(vars.Count);
 			var values = new List<Value>(vars.Count);
 
-			foreach (var kvp in vars) {
+			foreach (var kvp in vars)
+			{
 				keys.Add(kvp.Key);
 				values.Add(kvp.Value);
 			}
