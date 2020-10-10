b0c678c894cfd71fa0b4aed21801a638d7ac4e09
Ava Pek <exodrifter@gmail.com>
2020-10-10 14:28:27 -0500

    Change CharParser to use a predicate

--- Parser/Simple/CharParser.cs
+++ Parser/Simple/CharParser.cs
@@ -1,28 +1,45 @@
-﻿namespace Exodrifter.Rumor.Parser
+﻿using System;
+
+namespace Exodrifter.Rumor.Parser
 {
 	public class CharParser : Parser<char>
 	{
-		private readonly char ch;
+		private readonly Func<char, bool> predicate;
+		private readonly string expected;
 
 		public CharParser(char ch)
+			: this((other) => ch == other, ch.ToString()) { }
+
+		/// <summary>
+		/// Creates a new CharParser which succeeds on a predicate check.
+		/// </summary>
+		/// <param name="predicate">
+		/// The predicate to test.
+		/// </param>
+		/// <param name="expected">
+		/// The expected value (used in error messages).
+		/// </param>
+		public CharParser(Func<char, bool> predicate, string expected)
 		{
-			this.ch = ch;
+			this.predicate = predicate;
+			this.expected = expected;
 		}
 
 		public override Result<char> Parse(State state)
 		{
 			if (state.Source.Length <= state.Index)
 			{
-				return Result<char>.Error(state.Index, ch.ToString());
+				return Result<char>.Error(state.Index, expected);
 			}
 
-			if (state.Source[state.Index] == ch)
+			var ch = state.Source[state.Index];
+			if (predicate(ch))
 			{
 				var newState = state.AddIndex(1);
 				return Result<char>.Success(newState, ch);
 			}
 
-			return Result<char>.Error(state.Index, ch.ToString());
+			return Result<char>.Error(state.Index, expected);
 		}
 	}
 }
