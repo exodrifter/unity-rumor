c46364f1b40d417e9b1039fca06b46ea8f0d8d3f
Ava Pek <exodrifter@gmail.com>
2020-10-10 23:30:51 -0500

    Add docs and IndentIndex to State

--- Parser/State.cs
+++ Parser/State.cs
@@ -1,30 +1,77 @@
 ﻿namespace Exodrifter.Rumor.Parser
 {
+	/// <summary>
+	/// Contains the state of a parsing operation.
+	/// </summary>
 	public class State
 	{
+		/// <summary>
+		/// The contents of the source file that is being parsed.
+		/// </summary>
 		public string Source { get; }
+
+		/// <summary>
+		/// The current index in the source file the parser is pointing at.
+		/// </summary>
 		public int Index { get; }
 
+		/// <summary>
+		/// The index in the source file to check the current indentation level
+		/// against, if needed.
+		/// </summary>
+		public int IndentIndex { get; }
+
+		/// <summary>
+		/// Creates a new parser state.
+		/// </summary>
+		/// <param name="source">
+		/// The contents of the source to parse.
+		/// </param>
+		/// <param name="index">
+		/// The index where the parser should start.
+		/// </param>
 		public State(string source, int index = 0)
 		{
 			Source = source;
 			Index = index;
+			IndentIndex = 0;
 		}
 
 		/// <summary>
-		/// Makes a copy of another state except for the index.
+		/// Returns a new state with the index changed by the specified delta.
 		/// </summary>
-		/// <param name="state">The state to copy.</param>
-		/// <param name="index">The new index.</param>
-		public State(State state, int index = 0)
-		{
-			Source = state.Source;
-			Index = index;
-		}
-
+		/// <param name="indexDelta">
+		/// The amount to change the index.
+		/// </param>
+		/// <returns>
+		/// A new state with a modified index.
+		/// </returns>
 		public State AddIndex(int indexDelta)
 		{
-			return new State(this, Index + indexDelta);
+			return new State(Source, Index + indexDelta, IndentIndex);
 		}
+
+		/// <summary>
+		/// Sets the index of the location used for checking indentation levels
+		/// to the current index.
+		/// </summary>
+		/// <returns>
+		/// A new state with a modified index for checking indentation levels.
+		/// </returns>
+		public State SetIndent()
+		{
+			return new State(Source, Index, Index);
+		}
+
+		#region Internal
+
+		private State(string source, int index, int indentIndex)
+		{
+			Source = source;
+			Index = index;
+			IndentIndex = indentIndex;
+		}
+
+		#endregion
 	}
 }
